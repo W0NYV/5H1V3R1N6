aa2f3b2215ec69529b526af32f1d381fd753d449
w0nyv <nagano.tamawo77@gmail.com>
2023-01-24 13:19:13 +0900

    カメラワーク：スロースと補間の速度をMIDIで変えれるように

--- 5H1V3R1N6.Unity/Assets/W0NYV/CameraWork/CameraWork.cs
+++ 5H1V3R1N6.Unity/Assets/W0NYV/CameraWork/CameraWork.cs
@@ -7,7 +7,7 @@ namespace W0NYV.Shivering.CameraWork
     public class CameraWork : MonoBehaviour
     {
         [SerializeField] private GameObject _aimObj;
-        [SerializeField] private float _speed = 1f;
+        [SerializeField] private float _interpolateSpeed = 1f;
         [SerializeField] private float _aimDistance = 3f;
 
         float t = 0;
@@ -15,9 +15,20 @@ namespace W0NYV.Shivering.CameraWork
         Vector3 current;
         Vector3 rnd;
 
-        //微妙に動き続けるやつ
+        //sloth
+        [SerializeField] float _slothSpeed = 0.1f;
         bool canMove = false;
         Vector3 rndDir;
+
+        public void ChangeSlothSpeed(float v)
+        {
+            _slothSpeed = v * 0.9f + 0.1f;
+        }
+
+        public void ChangeSpeed(float v)
+        {
+            _interpolateSpeed = v * 4f + 1f;
+        }
 
         public void ChangeAimDistance(float v)
         {
@@ -65,11 +76,12 @@ namespace W0NYV.Shivering.CameraWork
             
             if(t<1f)
             {
-                t += Time.deltaTime * _speed;
+                t += Time.deltaTime * _interpolateSpeed;
                 transform.position = Vector3.Slerp(current, rnd, 3*t*t - 2*t*t*t);
             }
 
-            if(canMove)transform.position += rndDir * Time.deltaTime * 0.2f;
+            //sloth
+            if(canMove)transform.position += rndDir * Time.deltaTime * _slothSpeed;
 
             var aim = _aimObj.transform.position - transform.position;
             var look = Quaternion.LookRotation(aim, Vector3.up);
